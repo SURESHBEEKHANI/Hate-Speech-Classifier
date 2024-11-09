1f26c578af606c499a5d9d328ed7d504b727c903
sureshbeekhani <sureshbeekhani26@gmail.com>
2024-11-08 17:20:48 -0800

    create template of project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="SURESH BEEKHANI",
+    author_email="sureshbeekhani26@gamil.com",
+    packages=find_packages(),
+    install_requires=[],
+)
